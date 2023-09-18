5f78b86ec6565843a072258f8dc78e027547d241
ignacy <ignacpowala@gmail.com>
2023-09-18 18:50:44 +0200

    Summary

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,7 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 // Change code below this line
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const gallery = document.querySelector('.gallery');
 const galleryImageList = galleryItems
   .map(image => {
@@ -17,3 +18,26 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
 });
 console.log(galleryItems);
+
+// const fotoGallery = document.querySelector('.gallery');
+// const getFotoGallery = galleryItems
+//   .map(
+//     ({
+//       preview,
+//       original,
+//       description,
+//     }) => ` <a class="gallery__item" href="${original}">
+//     <img class="gallery__image" src="${preview}" alt="${description}" />
+// </a>`
+//   )
+//   .join(' ');
+// fotoGallery.insertAdjacentHTML('beforeend', getFotoGallery);
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionPosition: 'outside',
+//   captionDelay: 250,
+//   nav: false,
+//   close: false,
+//   showCounter: false,
+// });
+// console.log(galleryItems);
